14389eb90eb06da91fef643d5f8ecc9b814b91d8
DESKTOP-FO3JK3B\dibur <di.burd@yandex.ru>
2021-09-23 12:41:07 +0300

    Добавлен конфиг для RabbitMQ

--- microservices/ui-service/src/main/java/ru/dburdin/testjaspertemplates/ui/service/impl/DaoImpl.java
+++ microservices/ui-service/src/main/java/ru/dburdin/testjaspertemplates/ui/service/impl/DaoImpl.java
@@ -1,6 +1,9 @@
 package ru.dburdin.testjaspertemplates.ui.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.dburdin.testjaspertemplates.ui.model.dto.entity.ExpensesTypeEntity;
 import ru.dburdin.testjaspertemplates.ui.repository.ExpensesTypeRepository;
@@ -10,8 +13,13 @@ import java.util.List;
 
 @Service
 public class DaoImpl implements Dao {
+	private static final Logger LOGGER = LoggerFactory.getLogger(DaoImpl.class);
+	private final ExpensesTypeRepository expensesTypeRepository;
+
 	@Autowired
-	private ExpensesTypeRepository expensesTypeRepository;
+	public DaoImpl(ExpensesTypeRepository expensesTypeRepository) {
+		this.expensesTypeRepository = expensesTypeRepository;
+	}
 
 	@Override
 	public List<ExpensesTypeEntity> allExpensesType() {
